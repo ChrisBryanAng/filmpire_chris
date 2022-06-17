c9bc4c75b63dddeb9922b978646ddfbcd432a8b4
ChrisBryanAng <ChrisBryanAng@gmail.com>
2022-06-17 16:45:13 -0700

    Added ability to fetch and display corresponding movies by category or genre with previous slice.

--- src/features/currentGenreOrCategory.js
+++ src/features/currentGenreOrCategory.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
 export const genreOrCategory = createSlice({
   name: 'genreOrCategory',
   initialState: {
-    genreOrCategoryName: '',
+    genreIdOrCategoryName: '',
     page: 1,
     searchQuery: '',
   },
   reducers: {
     selectGenreOrCategory: (state, action) => {
-      console.log(action.payload);
-      // state.genreOrCategoryName =
+      state.genreIdOrCategoryName = action.payload;
+      // state.genreIdOrCategoryName =
     },
   },
 });
